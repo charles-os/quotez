ddab2d56e02430cb2edc484cd301c0c7ff7f32e6
chaloo56 <chaloo56@gmail.com>
2019-07-08 14:07:02 +0300

    initializing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteCreateFormComponent } from './quote-create-form/quote-create-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HighlightDirective,
+    DateCountPipe,
+    QuoteComponent,
+    QuoteCreateFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote.ts
@@ -0,0 +1,15 @@
+export class Quote {
+    showDetails: boolean;
+    constructor(
+      public id = 1,
+      public author: string,
+      public quote: string,
+      public postedBy: string,
+      public timePosted: Date,
+      public upVote: number,
+      public downVote: number
+  ) {
+      this.showDetails = false;
+    }
+  }
+  
